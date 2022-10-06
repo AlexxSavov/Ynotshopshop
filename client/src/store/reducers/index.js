d9d4616ef902e46cd53694b3dc58579dc815e0c3
Alex <Alex@Samanthas-MacBook-Pro.local>
2022-10-05 22:40:52 -0400

    add cart reducer

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import productsReducer from "./productsReducer";
+import cartReducer from "./cartReducer";
 
 const reducers = combineReducers({
     // reduers go here
     productsReducer,
+    cartReducer,
 });
 
 export default reducers;
